292164b2177f4cd20e37e4b0d3d16211c36f3fea
Jakub Netolický <it2220@sspu-opava.cz>
2026-06-08 08:30:29 +0200

    fix ip adresy serveru

--- archicosik_project/settings.py
+++ archicosik_project/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-abc123'
 DEBUG = True
-ALLOWED_HOSTS = ['localhost', '127.0.0.1',]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.1.218']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
